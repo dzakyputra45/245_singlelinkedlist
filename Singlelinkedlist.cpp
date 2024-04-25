f5338ecdbdc0b18c1b7381c6014085cd503c0c03
dzakyputra45 <dzakypratama45@gmail.com>
2024-04-25 14:05:45 +0700

    Menambahkan variable di dalam Node yang baru

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -10,5 +10,13 @@ struct Node {
 Node* START = NULL;
 
 void addNode() {
-    
+    int nim;
+    string nama;
+    Node* nodebaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodebaru->noMhs = nim;
+    nodebaru->name = nama;
 }
